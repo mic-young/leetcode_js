e417c906199e0b8ac40f5c2f166fa7ca6010efc7
sc <869101940@qq.com>
2024-09-15 14:02:57 +0800

    模板v1

--- 简单/1_计数器.js
+++ 简单/1_计数器.js
@@ -1,33 +1,58 @@
-// 计数器的实现方法
+// 2620.计数器
+// 给定一个整型参数 n，请你编写并返回一个 counter 函数。这个 counter 函数最初返回 n，每次调用它时会返回前一个值加 1 的值 ( n ,  n + 1 ,  n + 2 ，等等)。 
 
-// 基于闭包
+
+// ** solution
+// 
+// 使用闭包的特性,在外部函数定义一个变量,内部函数使用变量并返回.
 let createCounter = function (n) {
     let counter = n;
     return function () {
         counter += 1;
         return counter;
     }
-} 
-// 基于闭包实现的其他语法
-// 后缀递增
+}
+
+
+// ** post
+// 
+// 1 - 解释
+// 闭包返回的函数实现了封装,无法直接访问到内部的变量.
+// 类在不使用 #的前缀语法,能够访问到内部的counter.
+// 类生成的counter是复用的原型对象的函数,闭包则是重复生成新的counter.
+// 类的性能更好.闭包更简洁.
+
+// 2 - 题解
+// https://leetcode.cn/problems/counter/solutions/2487678/ji-shu-qi-by-leetcode-solution-xuwj/
+
+
+// ** solution
+// 
+// 1 - 后缀递增
+// 使用函数的参数作为counter,函数的参数也是函数内定义的变量
 let createCounter2 = function (n) {
     return function () {
         return n++;
     }
 }
-// 前缀递减递增
+
+// 2 - 前缀递减和递增
+// 更多余的写法
 let createCounter3 = function (n) {
     --n;
     return function () {
         return ++n;
     }
 }
-// 箭头函数后缀递增
+
+// 3 - 箭头函数后缀递增
+// 更简洁的写法
 let createCounter4 = function (n) {
     return () => n++;
 }
 
-// 基于类
+// 4 - 基于类
+// 类的写法
 class Adder {
     constructor(n) {
         this.counter = n;
@@ -37,11 +62,3 @@ class Adder {
         this.counter++;
     }
 }
-
-// 闭包返回的函数实现了封装,无法直接访问到内部的变量.
-// 类在不使用 #的前缀语法,能够访问到内部的counter.
-// 类生成的counter是复用的原型对象的函数,闭包则是重复生成新的counter.
-// 类的性能更好.闭包更简洁.
-
-// 题解
-// https://leetcode.cn/problems/counter/solutions/2487678/ji-shu-qi-by-leetcode-solution-xuwj/
--- 简单/2_计数器2.js
+++ 简单/2_计数器2.js
@@ -1,2 +1,16 @@
 // 计数器的实现2
 
+let createCounter=function (init){
+    let counter=init;
+    return {
+        increment(){
+            return counter++;
+        },
+        decrement(){
+            return counter--;
+        },
+        reset(){
+            return counter=init;
+        }
+    }
+}
